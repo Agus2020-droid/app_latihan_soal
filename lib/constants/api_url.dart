addb6b46883245e3b9a55aef13dcba94b6c8bb77
Agus2020-droid <abie.ang@gmail.com>
2022-08-14 15:31:57 +0700

    models json

---
+++ lib/constants/api_url.dart
@@ -0,0 +1,12 @@
+class ApiUrl {
+  static String baseUrl = "https://eskul.id/api/";
+  static String users = "users";
+  static String userRegistrasi = "users/registrasi";
+  static String userUpdateProfile = "users/update";
+
+  static String latihanMapel = "excercise/data_course";
+  static String latihanPaketSoal = "excercise/data_excercise";
+  static String latihanKerjakanSoal = "excercise/kerjakan";
+  static String latihanSubmitJawaban = "excercise/input_jawaban";
+  static String latihanSkor = "excercise/score_result";
+}
